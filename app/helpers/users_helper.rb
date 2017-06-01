2e670ac7b7698c2b8bd34384166812bd2eaf6f7f
tuananhhoang <montuananh@gmai.com>
2017-06-01 10:48:53 +0700

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def avatar_for user
+    avatar_id = Digest::MD5::hexdigest user.email.downcase
+    avatar_url = "https://en.gravatar.com/userimage/122389688/b22d9e4c2ece9eb07732561028910ea8.jpeg"
+    image_tag avatar_url, alt: user.name, class: "avatar"
+  end
 end
